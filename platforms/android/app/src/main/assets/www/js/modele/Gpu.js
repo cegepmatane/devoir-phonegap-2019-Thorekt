128542d4ae3f5cc664a0f3ae35351759f3841e27
Thorekt <35566455+Thorekt@users.noreply.github.com>
2019-11-05 21:09:55 -0500

    modele gpu

---
+++ platforms/android/app/src/main/assets/www/js/modele/Gpu.js
@@ -0,0 +1,7 @@
+var Gpu = function(nom, marque, frequence, nombreCoeur, id){
+    this.id = id;
+    this.nom = nom;
+    this.marque = marque;
+    this.frequence = frequence;
+    this.nombreCoeur = nombreCoeur;
+}
